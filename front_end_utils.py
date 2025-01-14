17bf8cb3212fe20353e1b0492381a35e73edebc0
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-14 03:35:11 -0500

    Update front_end_utils.py

--- front_end_utils.py
+++ front_end_utils.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def render_section_separator:
+def render_section_separator():
   # 
   st.markdown("<br>", unsafe_allow_html=True)
   st.markdown("<br>", unsafe_allow_html=True)
