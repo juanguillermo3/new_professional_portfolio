53d7c395677539282e61ebc74bf10b899b415c1b
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-20 15:11:48 -0500

    Update about_section.py

--- about_section.py
+++ about_section.py
@@ -12,7 +12,7 @@ class AboutSection:
             <p style="color: gray;">
             This portfolio showcases code samples I developed over more than five years as a Data Analyst and Data Mining Engineer across several projects. 
             It is quite eclectic as I am not focused on a single vertical but I hope it's representative of some core workflows I enjoy working in. Moreover, ongoing research aims at leveraging emerging technologies like 
-            Recommendation Systems (RecSys) and LLM-powered applications (LLM apps) to create practical software solutions to practical problem of workers at the laboral marketing, such as efficiently displaying content to an audience 
+            Recommendation Systems (RecSys) and LLM-powered applications (LLM apps) to create practical software solutions to practical problem of workers at the laboral market, such as efficiently displaying content to an audience 
             of potential clients or employers. Features like the RecSys are still under development, and related research can be found on my 
             <a href="{self.linkedin_profile}" target="_blank" style="color: #1f77b4; text-decoration: none;">LinkedIn profile</a>. 
             Some content is AI-generated for developmental purposes.
