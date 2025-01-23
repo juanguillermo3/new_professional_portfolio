521888b67ee66f4b902f56735e8afac2ffacb8c2
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-23 16:22:07 -0500

    Update rec_sys.py

--- rec_sys.py
+++ rec_sys.py
@@ -55,21 +55,20 @@ class RecommendationSystem:
             related_items_count = sum(1 for item in self.metadata_list if item['repo_name'].lower() == repo_name)
             project_item_counts[repo_name] = related_items_count
 
-            # Sort repos by ongoing status (True for ongoing first), then by item count
-            self.repos_metadata.sort(
-                key=lambda x: (
-                    not x.get("ongoing", False),  # False for ongoing (sorts first), True otherwise
-                    -project_item_counts.get(x["title"].lower(), 0)  # Descending by item count
-                )
+        # Sort repos by ongoing status (True for ongoing first), then by item count
+        self.repos_metadata.sort(
+            key=lambda x: (
+                not x.get("ongoing", False),  # False for ongoing (sorts first), True otherwise
+                -project_item_counts.get(x["title"].lower(), 0)  # Descending by item count
             )
-            
+        )
+        
         # Prepare titles for the selector with "(Ongoing)" appended if applicable
         self.project_titles = [
-            repo["title"] if not repo.get("ongoing", True) 
-            else f"{repo['title']} (Ongoing)"
+            f"{repo['title']} (Ongoing)" if repo.get("ongoing", False) else repo["title"]
             for repo in self.repos_metadata
         ]
-
+        
         # Map prettified titles to actual titles
         self.title_mapping = {
             self.prettify_title(title): repo["title"]
