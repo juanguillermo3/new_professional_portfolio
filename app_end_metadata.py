69150182bcd8f3a9f506e411139e8d5f7ee11adc
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-01-29 21:30:39 -0500

    Update app_end_metadata.py

--- app_end_metadata.py
+++ app_end_metadata.py
@@ -4,17 +4,20 @@ def load_repos_metadata():
         {
             "title": "lab_market_trends",
             "ongoing": True,
-            "tags": ["NLP", "labor market", "trends"]
+            "tags": ["labor market", "wages anlysis", "data engineering", "PySpark", "Selenium"]
         },
         {
             "title": "monkey_research",
             "ongoing": True,
-            "tags": ["ethology", "monkeys", "behavior"]
+            "tags": ["ethology research", "inferential statistics", "R Studio", "Visualization"]
+        },
+        {
+            "title": "new_professional_portfolio",
+            "ongoing": True,
+            "tags": ["online portfolio", "application development", "streamlit", "Recommendation Systems", "LLM apps"]
         },
         {
             "title": "predictive_analytics",
-            "ongoing": True,
-            "tags": ["ML", "algorithms", "research"]
+            "tags": ["Neural Networks", "Genetic Algorithms", , "Ensemble Learning"]
         },
-        # Add more as needed
     ]
