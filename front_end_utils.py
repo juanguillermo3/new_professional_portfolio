1c0188b92b88d365bbc76525f27457ac0435734a
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-07 15:32:37 -0500

    Update front_end_utils.py

--- front_end_utils.py
+++ front_end_utils.py
@@ -5,3 +5,21 @@ def render_section_separator():
   st.markdown("<br>", unsafe_allow_html=True)
   st.markdown("<br>", unsafe_allow_html=True)
   st.markdown("<br>", unsafe_allow_html=True)
+
+#
+def render_external_link_button(url, label, bg_color):
+    """Helper method to render an external link button with consistent styling."""
+    return f"""
+    <div style="display: flex; justify-content: center; margin-top: 5px;">
+        <a href="{url}" target="_blank" 
+           style="text-decoration: none; width: 200px; display: block; margin: 0 auto;">
+            <button style="background-color: {bg_color}; color: white; 
+                           border: none; padding: 10px 20px; 
+                           text-align: center; text-decoration: none; 
+                           font-size: 14px; cursor: pointer; 
+                           border-radius: 5px; width: 100%; margin: 0 auto;">
+                {label}
+            </button>
+        </a>
+    </div>
+    """
