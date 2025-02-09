76d5c0c9ce56ebbb20820ca345787b80fc33816d
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-08 22:39:18 -0500

    Update hero_area.py

--- hero_area.py
+++ hero_area.py
@@ -80,7 +80,9 @@ class HeroArea:
                 display: flex;
                 justify-content: center;
                 align-items: center;
-                height: 100%;
+                height: 100%;  /* Ensure the container takes up full height */
+                text-align: center;  /* Center the content horizontally as well */
+                padding: 0;  /* Remove padding if needed */
             }
             </style>""", unsafe_allow_html=True)
             for paragraph in self.quote:
