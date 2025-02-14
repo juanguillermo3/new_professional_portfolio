d449802eed490054f42d2f45b49f736535ce5c36
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-14 00:07:11 -0500

    Update rec_sys.py

--- rec_sys.py
+++ rec_sys.py
@@ -22,7 +22,7 @@ from git_api_utils import load_repos_metadata as load_github_metadata
 from app_end_metadata import load_repos_metadata as load_app_metadata
 from front_end_utils import render_section_separator, render_external_link_button, prettify_title, tags_in_twitter_style
 from media_carousel import MediaCarousel  # Assuming this is the correct import
-from visual_media import render_item_visual_content, VisualContentGallery, test_gallery_collection, GalleryCollection
+from visual_media import render_item_visual_content, VisualContentGallery, test_gallery_collection, GalleryCollection, test_gallery
 from front_end_for_recommended_content import html_for_item_data
 
 #
