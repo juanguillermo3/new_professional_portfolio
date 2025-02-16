35f08e8acd80519176409219eb1679372f5b7912
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-15 22:29:18 -0500

    Update app_end_metadata.py

--- app_end_metadata.py
+++ app_end_metadata.py
@@ -1,18 +1,6 @@
 # app_end_metadata.py
 def load_repos_metadata():
     return [
-         {
-            "title": "lab_market_trends",
-            "ongoing": True,
-            "tags": ["labor market", "wages analysis", "data engineering", "PySpark", "Selenium"],
-            "achieved_milestones": [
-                "Data pipeline established",
-                "First wage trend analysis published"
-            ],
-            "next_milestones": [
-                "Integrate real-time labor demand trends"
-            ]
-        },
         {
             "title": "monkey_research",
             "ongoing": True,
@@ -28,7 +16,27 @@ def load_repos_metadata():
             "tags": ["Neural Networks", "Genetic Algorithms", "Ensemble Learning"]
         },
         {
-            "title": "sales_forecasting_with_genetic_neural_networks",
-            "tags": ["Sales Forecasting", "Food Delivery", "Deep Neural Networks", "Genetic Optimization", "Feed Forward Neural Networks", "Recurrent Neural Networks"]
+            "title": "lab_market_trends",
+            "ongoing": True,
+            "tags": ["labor market", "wages analysis", "data engineering", "PySpark", "Selenium"],
+            "achieved_milestones": [
+                "Data pipeline established",
+                "First wage trend analysis published"
+            ],
+            "next_milestones": [
+                "Integrate real-time labor demand trends"
+            ]
         },
+        {
+        "title": "sales_forecasting_with_genetic_neural_networks",
+        "tags": ["Sales Forecasting", "Food Delivery", "Deep Neural Networks", "Genetic Optimization", "Feed Forward Neural Networks", "Recurrent Neural Networks"],
+        "achieved_milestones": [
+            "Built an ETL pipeline to aggregate sales counts into hourly and 15-minute intervals.",
+            "Developed services for feature extraction and ML sample delivery for forecasting.",
+            "Implemented traditional forecasting models (ARIMA, Exponential Smoothing).",
+            "Developed AI-based forecasting models (Deep Neural Networks, Recurrent Neural Networks).",
+            "Conducted research and custom-built a Genetic Optimization algorithm to fine-tune neural networks using core Python with GPT assistance."
+        ],
+        "next_milestones": []
+    }
     ]
