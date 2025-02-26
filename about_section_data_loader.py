c0852b9c94365d07f452fda688d8c5a3092671e8
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-25 23:26:11 -0500

    Update about_section_data_loader.py

--- about_section_data_loader.py
+++ about_section_data_loader.py
@@ -22,8 +22,5 @@ def load_general_info():
         f"This portfolio showcases code samples I have developed over more than {num_years} years as a Data Analyst "
         "and ML Consultant across various projects. You'll likely find my work quite eclectic, as I have explored topics "
         "as varied as the impact of gender on the Colombian labor market and scientific research on monkey behavior "
-        "to high-performance sales forecasting for food delivery services. My recurring interest nevertheless has always "
-        "been the modernization of the data analysis pipeline through cutting-edge techniques, such as flexible ML-based "
-        "inference, software and algorithmic automation, using NLP in latent semantic spaces, and, more recently, solving "
-        "data analysis tasks through agency formation within LLM applications."
+        "to high-performance sales forecasting for food delivery services."
     )
