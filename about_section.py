5be9267dd76d86ddf6de25fb5854c19c298e1167
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-02-28 00:25:58 -0500

    Update about_section.py

--- about_section.py
+++ about_section.py
@@ -1,3 +1,8 @@
+"""
+title: About section
+description: 
+"""
+
 import streamlit as st
 from portfolio_section import PortfolioSection
 from about_section_data_loader import load_general_info, load_key_interest, load_key_hypothesis, load_dev_environment
@@ -7,36 +12,7 @@ from about_section_data_loader import (
     load_key_hypothesis, 
     load_dev_environment
 )
-
-
-def exceptional_but_subtle_quote(markdown_text: str):
-    """
-    Render a subtle notable quote with:
-    - 5% left indentation.
-    - Soft shadow effect on bottom and right.
-    - No special font styling (blends naturally).
-    - Properly parsed markdown inside the styled container.
-    - Minimal spacing at the bottom for clarity.
-    - Vertically centered text.
-    """
-    subtle_style = f"""
-        <div style="
-            padding: 10px;
-            margin-left: 5%;
-            border-radius: 6px;
-            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
-            display: flex;
-            align-items: center;
-            min-height: 60px;
-        ">
-            {markdown_text}
-
-    """
-    
-    st.markdown(subtle_style, unsafe_allow_html=True)
-
-
-
+from exceptional_quote import exceptional_but_subtle_quote
 
 class AboutSection(PortfolioSection):
 
