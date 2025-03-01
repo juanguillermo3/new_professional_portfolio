2a9d21116dd7c3b9411be6f41512d651b822afec
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-01 16:53:52 -0500

    Update rec_sys.py

--- rec_sys.py
+++ rec_sys.py
@@ -28,6 +28,7 @@ from media_carousel import MediaCarousel  # Assuming this is the correct import
 from visual_media import  VisualContentGallery
 from front_end_for_recommended_content import html_for_item_data, html_for_milestones_from_project_metadata
 from portfolio_section import PortfolioSection
+from exceptional_ui import apply_custom_tooltip
 
 #
 # (0) ancillary function to merge metadata about underlyng items
