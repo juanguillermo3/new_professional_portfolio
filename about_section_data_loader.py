4bcd2aa7d24ea028938408c1ae50ecce42f9505c
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-01 17:56:19 -0500

    Update about_section_data_loader.py

--- about_section_data_loader.py
+++ about_section_data_loader.py
@@ -22,7 +22,7 @@ def load_general_info():
         str: A brief description of the portfolio's purpose and scope.
     """
     return (
-        "This portfolio showcases code samples I have developed over more than {num_years} years as a Data Analyst "
+        f"This portfolio showcases code samples I have developed over more than {num_years} years as a Data Analyst "
         "and ML Consultant across various projects. You'll likely find my work quite eclectic, as I have worked on a range "
         "of subjects as varied as the role of gender in the Colombian labor market, ethology research on monkey behavior, "
         "sales forecasting for delivery services, recommendation of optimal store placement, setup of key dashboards/reporting "
