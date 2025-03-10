57fe98009e6f5574cc9352b1b82767ff5c8f7d1c
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-10 15:25:19 -0500

    Update rec_sys.py

--- rec_sys.py
+++ rec_sys.py
@@ -206,6 +206,9 @@ class RecommendationSystem(PortfolioSection):
         
         #st.markdown("<br><br>", unsafe_allow_html=True)
 
+        card_id = id_from_item_data(rec) + "_card"
+        st.markdown(_custom_tooltip_with_frost_glass_html(card_id, rec['title'] ), unsafe_allow_html=True)
+
         if False:
           card_id = id_from_item_data(rec) + "_card"
           MOCK_INFO_PREFIX = "Info: "
