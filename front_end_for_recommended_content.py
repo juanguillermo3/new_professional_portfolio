394a2d7e8343ace3185af8a67f4762cfb032e235
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-10 17:44:50 -0500

    Update front_end_for_recommended_content.py

--- front_end_for_recommended_content.py
+++ front_end_for_recommended_content.py
@@ -61,7 +61,7 @@ def html_for_item_data(
         <div style="background-color: {background_color}; border: {border_style}; 
                     border-radius: 10px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
                     padding: 10px; height: {card_height}; 
-                    display: flex; flex-direction: column; justify-content: space-between; position: relative; overflow: visible;"">
+                    display: flex; flex-direction: column; justify-content: space-between; position: relative; overflow: visible;">
 
             <div style="background-color: rgba(255, 255, 255, 0.7); 
                         padding: 5px 10px; border-radius: 10px 10px 0 0; 
