07ef4d667d540233b267e751ad21cbafc2e2fcca
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-11 21:18:14 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -200,6 +200,11 @@ def load_detailed_offering(id_pattern="offering-{}", colors=["#f0f0f0", "#ffffff
             "title": "Inferential Statistics & High-Performance Predictive Analytics",
             "description": "I research and implement techniques for regression, classification, and forecasting use cases, \
             with applications ranging from macroeconomic and financial forecasting to microdata predictions in various systems.",
+            "skills": [
+                "Strong grip on the ML and predictive analytics development cycle", 
+                "Foundations on Artificial Intelligence for predictive models using Neural Networks", 
+                "Development of search algorithms to discover most performant algorithms" 
+            ]
         },
         {
             "title": "Software & Application Development for Inference Distribution",
