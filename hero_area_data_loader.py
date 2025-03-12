df5f4a2227193c0ab09e8443118fc2e67c09e4da
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-12 00:18:23 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -127,7 +127,7 @@ def load_detailed_offering(id_pattern="offering-{}", colors=["#f0f0f0", "#ffffff
 
         # Insert the tooltip for the list of technical skills
         if "skills" in offer:
-            offering_html += "<span>"+html_for_tooltip_from_large_list(offer["skills"], label="Technical Skills", element_id=element_id, color="#555", emoji="🏅")+"</span>"
+            offering_html += html_for_tooltip_from_large_list(offer["skills"], label="Technical Skills", element_id=element_id, color="#555", emoji="🏅")
             tooltip_elements.append(element_id)  # Store the ID for hover logic
 
         offering_html += '</li>'
