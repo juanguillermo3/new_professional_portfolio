f5bb9c09d40709241841618d6ca193f209f3581e
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-20 19:52:04 -0500

    Update front_end_for_recommended_content.py

--- front_end_for_recommended_content.py
+++ front_end_for_recommended_content.py
@@ -110,15 +110,15 @@ def html_for_item_data(
 
     # (1) GitHub Button
     if "url" in rec and rec["url"]:
-        buttons_html.append(button_fabric.render("GitHub", rec["url"], "#333"))
+        buttons_html.append(button_fabric.render_button("GitHub", rec["url"], "#333"))
     
     # (2) Google Sheets Button
     if "report_url" in rec and rec["report_url"]:
-        buttons_html.append(button_fabric.render("Sheets", rec["report_url"], "#34A853"))
+        buttons_html.append(button_fabric.render_button("Sheets", rec["report_url"], "#34A853"))
     
     # (3) Google Colab Button
     if "colab_url" in rec and rec["colab_url"]:
-        buttons_html.append(button_fabric.render("Colab Notebook", rec["colab_url"], "#F9AB00"))
+        buttons_html.append(button_fabric.render_button("Colab Notebook", rec["colab_url"], "#F9AB00"))
     
     buttons_html="\n".join(buttons_html)
 
