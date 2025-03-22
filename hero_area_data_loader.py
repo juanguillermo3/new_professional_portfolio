1b71a785df5d358cefdb4fdcfda11c91730ff6af
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-22 16:30:16 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -45,7 +45,7 @@ def load_quote():
         "My recurring interest has been the <b>modernization of data analysis</b> "  
         "through cutting-edge techniques, such as flexible statistical inference powered by <b>Machine Learning</b>, "  
         "streamlining <em>core business</em> workflows through <b>software development</b> and <b>algorithmic automation</b>, "
-        "handling datasets with <b>modern data tech stacks</b>, "  
+        "handling <em>unruly datasets</em> with <b>modern data tech stacks</b>, "  
         "grouping data by <em>meaning</em> with <b>Natural Language Processing</b>, "  
         "and dynamically enabling <em>decision-making</em> through the <b>agency of LLM applications</b>. ",  
 
