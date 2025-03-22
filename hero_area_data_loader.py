1c02f9d330dbf9e15cd2aca9abb1011c4f9c5267
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-22 14:49:02 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -81,7 +81,7 @@ def load_detailed_offering(id_pattern="offering-{}", colors=["#f0f0f0", "#ffffff
             "title": "⚡ 1. High-Performance Prediction of Key Business Outcomes",
             
             "description": "I implement <em>statistically driven inference</em> of predictive patterns powered by state-of-the-art <em>Machine Learning</em> "
-                           "and <em>Deep Learning</em> algorithms for prediction and forecasting settings. Applications range from forecasting "
+                           "and <em>Deep Learning</em> algorithms. Applications range from forecasting "
                            "the macroeconomic/financial environment to <em>fine-grained predictions</em> from business micro-data (sales forecasting, churn modeling, engagement models).",                       
             "skills": [
                 "Familiarity with predictive algorithms for regression, classification, and forecasting problems.", 
