c437ef840a6f575e825150c65aeb642e8d59be49
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-26 18:33:40 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -162,7 +162,7 @@ def custom_html_for_offerings(id_pattern="offering-{}", colors=["#f0f0f0", "#fff
 
         # Ancillary span with hidden full description
         if full_description:
-            offering_html += f' <span class="hoover-{element_id}" style="display: none;">{full_description}</span>'
+            offering_html += f' <span class="hoover-{element_id}" style="display: none;">{full_description}</span>'+hover_text_component()
             style_block += f"#{element_id}:hover .hoover-{element_id} " + "{ display: inline; }\n"
 
         if "skills" in offer:
