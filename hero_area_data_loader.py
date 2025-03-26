ee02fc533e1fbeea574008f93cae6320076d586b
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-26 18:38:40 -0500

    Update hero_area_data_loader.py

--- hero_area_data_loader.py
+++ hero_area_data_loader.py
@@ -162,8 +162,8 @@ def custom_html_for_offerings(id_pattern="offering-{}", colors=["#f0f0f0", "#fff
 
         # Ancillary span with hidden full description
         if full_description:
-            offering_html += f' <span class="hoover-{element_id}" style="display: none;">{full_description}</span>\n'
-            style_block += f"#{element_id}:hover .hoover-{element_id} " + "{ display: inline; }"
+            offering_html += f' <span class="hoover-{element_id}" style="display: none;">{full_description}</span>'
+            style_block += f"#{element_id}:hover .hoover-{element_id} " + "{ display: inline; }\n"
 
         if "skills" in offer:
             tooltip_html, unique_id = html_for_tooltip_from_large_list(
@@ -186,7 +186,7 @@ def custom_html_for_offerings(id_pattern="offering-{}", colors=["#f0f0f0", "#fff
     style_block += "</style>\n"
 
     # Return HTML with dynamically generated styles
-    return style_block + offering_html , tooltip_ids
+    return style_block + offering_html, tooltip_ids
 
     
 
