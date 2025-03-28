8140229321f90643822bfcae7f77c50e60b2dd8d
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-28 11:27:49 -0500

    Update hero_area.py

--- hero_area.py
+++ hero_area.py
@@ -171,7 +171,7 @@ class HeroArea:
         
         st.markdown(self.detailed_offering, unsafe_allow_html=True)
         for id in self.ids:
-            st.markdown(setup_tooltip_behavior(id), unsafe_allow_html=True)          
+            #st.markdown(setup_tooltip_behavior(id), unsafe_allow_html=True)          
         self.render_code_samples()
 
     def _render_quote(self):
