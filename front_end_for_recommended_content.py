d6b924f7376120b6370f2ce242e8852093d1726e
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-03-31 16:14:34 -0500

    Update front_end_for_recommended_content.py

--- front_end_for_recommended_content.py
+++ front_end_for_recommended_content.py
@@ -340,7 +340,6 @@ def html_for_item_data(
     <div id="{card_id}" class="recommendation-card">
         <div class="recommendation-title">
             {raw_title}
-            #<a href="{rec["url"] if "url" in rec else ""}" target="_blank" class="github-btn"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub"></a>
         </div>  
         <div>
             <a href="{rec["url"] if "url" in rec else ""}" target="_blank" class="github-btn"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub"></a>
