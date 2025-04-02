16c0e5ec7875bbd0b55ffddf9d164199d3d274c4
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-04-02 15:56:02 -0500

    Update hero_area.py

--- hero_area.py
+++ hero_area.py
@@ -378,6 +378,9 @@ class HeroArea:
     
         offering_html += '</ul>'
         st.markdown(offering_html, unsafe_allow_html=True)
+
+        style_block += "</style>"
+        st.markdown(style_block, unsafe_allow_html=True)
     
         st.markdown(f"""
           <style>
@@ -422,8 +425,7 @@ class HeroArea:
           """,
           unsafe_allow_html=True)
 
-      style_block += "</style>"
-      st.markdown(style_block, unsafe_allow_html=True)
+
 
 
 # Instantiate and render HeroArea with data loaded from the loader functions
