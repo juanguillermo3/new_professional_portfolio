2532458b3663b37b534878820cf36fb7001bc0ec
juanguillermo3 <135280741+juanguillermo3@users.noreply.github.com>
2025-05-01 17:13:34 -0500

    Update rec_sys.py

--- rec_sys.py
+++ rec_sys.py
@@ -45,9 +45,13 @@ MOCK_INFO_PREFIX = os.getenv("MOCK_INFO", "[MOCK INFO]")
 # Instantiation of the semantic retriever -> helps to filter projects by meaning
 #
 
-from project_retrieval import SemanticRetriever
-project_retriever=SemanticRetriever("index/projects.index","index/metadata.json")
-code_retriever=SemanticRetriever("index/modules_index.index","index/modules_metadata.json")
+#from project_retrieval import SemanticRetriever
+#project_retriever=SemanticRetriever("index/projects.index","index/metadata.json")
+#code_retriever=SemanticRetriever("index/modules_index.index","index/modules_metadata.json")
+
+from semantic_retriever import SemanticRetriever
+project_retriever=SemanticRetriever("new_project_embeddings")
+code_retriever=SemanticRetriever("new_samples_embeddings")
 
 #
 # (0) ancillary function to merge metadata about underlyng items
